282c0a9cbe59cd2bd93208edb9f1149972c36c93
Lucas Alves <lucasacod@hotmail.com>
2024-05-19 15:55:36 -0300

    Initial commit

---
+++ src/main/java/dio/lucas/LucasDioSantanderApplication.java
@@ -0,0 +1,13 @@
+package dio.lucas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasDioSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucasDioSantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/lucas/LucasDioSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.lucas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucasDioSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
